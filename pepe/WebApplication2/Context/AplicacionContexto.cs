66dbfd7648ff2488fa726883f4a53c9ba0897d53
raziel <raziel.ascui@uab.edu.bo>
2023-10-13 15:55:35 +0200

    tarea

---
+++ pepe/WebApplication2/Context/AplicacionContexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+using WebApplication2.Modelss;
+using WebApplication2.Modelsss;
+
+namespace WebApplication2.Context
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto
+            (DbContextOptions<AplicacionContexto> options)
+            : base(options) { }
+
+        public DbSet<Estudiante> estudiante { get; set; }
+        public DbSet<Docente> docente { get; set; }
+        public DbSet<Universidad> universidad { get; set; }
+    }
+}
---
+++ pepe/WebApplication2/Models/Universidad.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Modelsss
+{
+    public class Universidad
+    {
+        [Key]
+        public int id_universidad { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
