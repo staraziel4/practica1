f6a2d044b4cad350f2e5cf93b9ca061e70c48a64
raziel <raziel.ascui@uab.edu.bo>
2023-10-16 16:28:36 +0200

    agregar modelo materias

---
+++ pepe/WebApplication2/Models/Materia.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Modelssss
+{
+    public class Materia
+    {
+        [Key]
+        public int id_materia { get; set; }
+        public string nombre { get; set; }
+        public int id_docente { get; set; }
+    }
+}
